570afc747b678d54d769dc2811b52dc462a72c28
Rafael Piazdecki <rafael.piazdecki@b2net.pl>
2016-07-11 20:34:54 +0200

    finished product specs:
    find_by_title, in_stock?, in_stock methods
    properties title, price stock

--- lib/product.rb
+++ lib/product.rb
@@ -1,16 +1,29 @@
 class Product
-  attr_reader :title
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
-    #@@products << self
 	@title = options[:title]
+	@price = options[:price]
+	@stock = options[:stock]
 	add_to_products
+  end
+  
+  def in_stock?
+    @stock > 0
   end
 
   def self.all
     @@products
+  end
+  
+  def self.find_by_title(title)
+    @@products.find{|product| product.title === title}
+  end
+  
+  def self.in_stock
+    @@products.select{|product| product.in_stock?}
   end
   
   private
