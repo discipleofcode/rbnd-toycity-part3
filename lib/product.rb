7854282f8791d8996b485b437ee051c3393b59b3
Rafael Piazdecki <rafael.piazdecki@b2net.pl>
2016-07-11 19:39:45 +0200

    throwing DuplicateProductError on product duplicates (title)

--- lib/product.rb
+++ lib/product.rb
@@ -1,11 +1,25 @@
 class Product
-@@products = []
+  attr_reader :title
+
+  @@products = []
 
   def initialize(options={})
-    @@products << self
+    #@@products << self
+	@title = options[:title]
+	add_to_products
   end
 
   def self.all
     @@products
+  end
+  
+  private
+  
+  def add_to_products
+    if (@@products.any?{|product| product.title === @title})
+      raise DuplicateProductError, "#{@title} already exists."
+	else  
+      @@products << self
+    end
   end
 end
